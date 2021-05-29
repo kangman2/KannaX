182d0b8a3350d5d4a5f9b611cdcaaca097a839fa
fnix <49885720+fnixdev@users.noreply.github.com>
2021-05-29 17:30:41 -0300

    Update pmpermit.py

--- kannax/plugins/utils/pmpermit.py
+++ kannax/plugins/utils/pmpermit.py
@@ -24,8 +24,8 @@ PMPERMIT_MSG = {}
 pmCounter: Dict[int, int] = {}
 allowAllFilter = filters.create(lambda _, __, ___: Config.ALLOW_ALL_PMS)
 noPmMessage = bk_noPmMessage = (
-    "`Hey {mention}. This is  A.I. Security Protocol [⚠️](https://telegra.ph/file/5807c69bf40fb39621b4a.mp4).`\n\n"
-    "`Meu mestre não aceita mensagem de estranhos.\n"
+    "`Hey {mention}. This is  A.I. Security Protocol` [⚠️](https://telegra.ph/file/5807c69bf40fb39621b4a.mp4).\n\n"
+    "`Meu mestre não aceita mensagem de estranhos.`\n"
     "`Espere que ele aprove você.`"
 )
 blocked_message = bk_blocked_message = "**You were automatically blocked**"
