8f6f15a907ecdcee57173fb391a90c31ea8b0ad3
fnix <49885720+fnixdev@users.noreply.github.com>
2021-06-17 01:12:00 -0300

    Update genStrSession.py

--- tools/genStrSession.py
+++ tools/genStrSession.py
@@ -21,13 +21,13 @@ if os.path.isfile("config.env"):
 async def genStrSession() -> None:  # pylint: disable=missing-function-docstring
     async with Client(
             "KannaX",
-            api_id=int(os.environ.get("API_ID") or input("Enter Telegram APP ID: ")),
-            api_hash=os.environ.get("API_HASH") or input("Enter Telegram API HASH: ")
+            api_id=int(os.environ.get("API_ID") or input("Insira Telegram APP ID: ")),
+            api_hash=os.environ.get("API_HASH") or input("Insira Telegram API HASH: ")
     ) as kannax:
-        print("\nprocessing...")
+        print("\nprocessando...")
         await kannax.send_message(
             "me", f"#KannaX #HU_STRING_SESSION\n\n```{await kannax.export_session_string()}```")
-        print("Done !, session string has been sent to saved messages!")
+        print("Pronto !, string de sessão foi enviada para mensagens salvas!")
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(genStrSession())
