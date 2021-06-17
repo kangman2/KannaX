8696b3afc891c31de47cd2e82a67161fa7df4406
fnix <49885720+fnixdev@users.noreply.github.com>
2021-06-17 01:10:25 -0300

    Update __main__.py

--- tests/__main__.py
+++ tests/__main__.py
@@ -16,8 +16,8 @@ from kannax import kannax
 async def _worker() -> None:
     chat_id = int(os.environ.get("CHAT_ID") or 0)
     type_ = 'unofficial' if os.path.exists("../kannax/plugins/unofficial") else 'main'
-    await kannax.send_message(chat_id, f'`{type_} build completed !`')
+    await kannax.send_message(chat_id, f'`{type_} build concluida !`')
 
 if __name__ == "__main__":
     kannax.begin(_worker())
-    print('KannaX test has been finished!')
+    print('O teste KannaX foi concluído!')
